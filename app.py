531b97dc512fbed5cb28670e449eb3747fa627e0
Nikhil Bollineni <nikhilbollineni@mac.speedport.ip>
2025-02-08 12:49:22 +0100

    added profile page line

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ import streamlit as st
 st.header("Hello")
 
 st.write("Welcome to my page")
+<<<<<<< HEAD
 st.header("profile") # added this line using online service 
+=======
+
+
+
+st.write("This is profile page -> where you can view all my experience")
+>>>>>>> a179254 (added a profile line)
